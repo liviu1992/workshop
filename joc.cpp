bb271c630f8ad6c75cb0cf77c150fda7a82fd54d
liviu1992 <miron_liviu92@yahoo.com>
2014-06-27 08:11:42 +0300

    am inceput sa refactorizez

--- joc.cpp
+++ joc.cpp
@@ -8,8 +8,8 @@
 #include "Matrix.cpp"
 #include "RAPIDXML\rapidxml_utils.hpp"
 #include "RAPIDXML\rapidxml.hpp"
-
-
+#include "Utils.cpp"
+#include "Shapes.cpp"
 
 /*
 	AICI SE AFLA TIPURILE DE SETARI PE CARE LE PUTEM 
@@ -35,6 +35,11 @@ float speedSquare;
 int g_gl_width = 640;
 int g_gl_height = 480;
 int g_gl_fullscreen = 0;
+
+
+	
+
+
 
 /*
 	INCARCA SETARILE JOCULUI DIN FISIERUL settings.xml
@@ -82,30 +87,13 @@ void loadSettings(){
 		counter++;
 
 
-		//std::cout << "Nume: " << name << " Valoare: " << value << std::endl;
 	}
 
 }
 
- // ....................Calculam FPS-ul si il afisam in titlul ferestrei.................
- void _update_fps_counter (GLFWwindow* window) {
-  static double previous_seconds = glfwGetTime ();
-  static int frame_count;
-  double current_seconds = glfwGetTime ();
-  double elapsed_seconds = current_seconds - previous_seconds;
-  if (elapsed_seconds > 0.25) {
-    previous_seconds = current_seconds;
-    double fps = (double)frame_count / elapsed_seconds;
-    char tmp[128];
-    sprintf (tmp, "opengl @ fps: %.2f", fps);
-    glfwSetWindowTitle (window, tmp);
-    frame_count = 0;
-  }
-  frame_count++;
-}
+ 
 
 void glfw_error_callback (int error, const char* description) {
-  //gl_log_err ("GLFW ERROR: code %i msg: %s\n", error, description);   <<< nu functioneaza
   std::cout << "GLFW ERROR: code "<<error <<" msg: "<<description << std::endl;
 }
 
@@ -115,29 +103,6 @@ void glfw_window_size_callback (GLFWwindow* window, int width, int height) {
   
   /* putem modifica aici matricile de perspectiva */
 }
-
-char * LoadFileInMemory(const char *filename)
-{
-	int size = 0;
-	char *buffer = NULL;
-	FILE *f = fopen(filename, "rb");
-	if (f == NULL)
-	{
-		return NULL;
-	}
-	fseek(f, 0, SEEK_END);
-	size = ftell(f);
-	fseek(f, 0, SEEK_SET);
-	buffer = new char[size + 1];
-	if (size != fread(buffer, sizeof(char), size, f))
-	{
-		delete[] buffer;
-	}
-	fclose(f);
-	buffer[size] = 0;
-	return buffer;
-}
-
 
 int main () {
 	loadSettings();
@@ -190,90 +155,35 @@ int main () {
   glewInit ();
 
   // Vedem versiunile
-  const GLubyte* renderer = glGetString (GL_RENDERER); //renderer string
-  const GLubyte* version = glGetString (GL_VERSION); // version string
-  printf ("Renderer: %s\n", renderer);
-  printf ("OpenGL version supported %s\n", version);
+ 
+  Utils::showOpengGLInfo();
 
   /*
 	MAI JOS INCARC SHADERELE, LE COMPILEZ SI LE ALOC PROGRAMELOR
   */
 
-  const char * vertex_shader = LoadFileInMemory("../data/vertexShaderT.glsl");
-  const char * fragment_shaderGreen = LoadFileInMemory("../data/fragmentShaderGreen.glsl");
-  const char * fragment_shader = LoadFileInMemory("../data/fragmentShader.glsl");
+  
 
-  GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vs, 1, &vertex_shader, NULL);
-  glCompileShader(vs);
+  GLuint vs =Utils::loadShader("../data/vertexShaderT.glsl", GL_VERTEX_SHADER);
+  GLuint fsg =Utils::loadShader("../data/fragmentShaderGreen.glsl", GL_FRAGMENT_SHADER);
+  GLuint fs =Utils::loadShader("../data/fragmentShader.glsl", GL_FRAGMENT_SHADER);
 
-  GLuint fsg = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fsg, 1, &fragment_shaderGreen, NULL);
-  glCompileShader(fsg);
 
-  GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fs, 1, &fragment_shader, NULL);
-  glCompileShader(fs);
-
-  GLuint shader_programme = glCreateProgram();
-  glAttachShader(shader_programme, vs);
-  glAttachShader(shader_programme, fs);
-  glLinkProgram(shader_programme);
-
-  GLuint shader_programmeGreen = glCreateProgram();
-  glAttachShader(shader_programmeGreen, vs);
-  glAttachShader(shader_programmeGreen, fsg);
-  glLinkProgram(shader_programmeGreen);
-
-  delete[] vertex_shader;
-  delete[] fragment_shader;
-  delete[] fragment_shaderGreen;
-
+  GLuint shader_programme = Utils::makeProgram(vs, fs);
+  GLuint shader_programmeGreen = Utils::makeProgram(vs, fsg);
+ 
   /*
 	VARIABILE NECESARE DESENARII CERCULUI
+ */
+  
+
+  /*
+		CREEZ CERCUL DANDU-I CA PARAMETRII NUMARUL DE VARFURI SI RAZA
   */
 
-   GLint num_of_triangles=num_of_vertices;
-   GLint num_of_points = 3 * num_of_triangles+1;
-   GLfloat radius = 0.3;
-   GLint buffer_size=num_of_points*2;
-   const GLfloat PI = 3.141;
-
-   GLfloat *vertex_buffer = new GLfloat[buffer_size+12*2];
-	/*
-		MAI JOS POPULEZ BUFFERUL CU TRIUNGHIURILE CARE VOR COMPUNE CERCUL
-	*/
-   
-   vertex_buffer[0]=0;
-   vertex_buffer[1]=0;
-
-  float angle = PI*2*1/num_of_triangles;
-
-   vertex_buffer[2]=radius*std::cos(angle);
-
-   vertex_buffer[3]=radius*std::sin(angle);
-
-   angle = PI*2*2/num_of_triangles;
-
-   vertex_buffer[4]=radius*std::cos(angle);
-
-   vertex_buffer[5]=radius*std::sin(angle);
-
-	int c;
-	for (int i=1; i<num_of_triangles; i++){
-		angle = PI*2*(2+i)/num_of_triangles;
-
-		vertex_buffer[i*6]=0;
-		vertex_buffer[i*6+1]=0;
-		vertex_buffer[i*6+2]=vertex_buffer[i*6-2];
-		vertex_buffer[i*6+3]=vertex_buffer[i*6-1];
-		vertex_buffer[i*6+4]=radius*std::cos(angle);
-		vertex_buffer[i*6+5]=radius*std::sin(angle);
-		c=i*6+5;
-	}
-	vertex_buffer[c-1]=vertex_buffer[2];
-	vertex_buffer[c]=vertex_buffer[3];
-
+  GLfloat *vertex_buffer=Shapes::createCircle(num_of_vertices, 0.2);
+  int circle_buffer_size = (3*num_of_vertices+1)*2;	   //spune ca de mult ocupa cercul 
+  int counter = 6*(num_of_vertices-1)+5;           //spune de unde incep vertecsii celorlalte forme
 	/*
 		PREGATESC CELELALTE CORPURI
 	*/
@@ -303,14 +213,14 @@ int main () {
   */
   
   for (int i=0; i<24; i++){
-	  vertex_buffer[c+1+i] = shapes_buffer[i];
+	  vertex_buffer[counter+1+i] = shapes_buffer[i];
   }
 
   // Generam un buffer in memoria video si scriem in el punctele din ram
   GLuint vbo = 0;
   glGenBuffers(1, &vbo); // generam un buffer 
   glBindBuffer(GL_ARRAY_BUFFER, vbo); // setam bufferul generat ca bufferul curent 
-  glBufferData(GL_ARRAY_BUFFER, (buffer_size+2 * 12) * sizeof (float), vertex_buffer, GL_STATIC_DRAW); //  scriem in bufferul din memoria video informatia din bufferul din memoria RAM
+  glBufferData(GL_ARRAY_BUFFER, (circle_buffer_size+2 * 12) * sizeof (float), vertex_buffer, GL_STATIC_DRAW); //  scriem in bufferul din memoria video informatia din bufferul din memoria RAM
 
   delete[] vertex_buffer;
   //delete[] shapes_buffer;
@@ -332,34 +242,23 @@ int main () {
 		starea obiectelor
 
 	*/
-
 	float circleX=0.5;
 	float circleY=0;
-
 	float squareX=0;
 	float squareY=-0.8;
-
 
 	/*
 		Mai jos sunt variabilele pe care le voi folosi sa calculez 
 		durata unui frame
 	*/
+	GLint mat =glGetUniformLocation(shader_programme, "Matrix");
 
 	float lastTime = glfwGetTime();
 	float newTime;
-	float frameTime;
-
-
-
-	GLint mat =glGetUniformLocation(shader_programme, "Matrix");
-
-
-	
-
-	
+	float frameTime;	
 
 	while (!glfwWindowShouldClose(window)) {
-		_update_fps_counter (window);
+		Utils::_update_fps_counter (window);
 
 		/*
 			Fac viteza de miscare sa fie independenta de framerate.
@@ -390,25 +289,20 @@ int main () {
 	  // facem bind la vertex buffer
 	  glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	  // draw points 0-3 from the currently bound VAO with current in-use shader
-	  glDrawArrays(GL_TRIANGLES, 0, num_of_triangles*3);
-
-	 // glUseProgram(NULL);
+	  glDrawArrays(GL_TRIANGLES, 0, num_of_vertices*3);
 
 	  glUseProgram(shader_programme);
-	 // move(translationMatrix, -0.8, -0.9, 0);
 
 	  m.translateTo(-0.8, -0.9);
 	  glUniformMatrix4fv(mat, 1, GL_FALSE, m.getData()); 
-	  glDrawArrays(GL_TRIANGLES, num_of_triangles*3, 3);
+	  glDrawArrays(GL_TRIANGLES,  num_of_vertices*3, 3);
 
 	  glUseProgram(shader_programmeGreen);
-
-	  //move(translationMatrix, -0.7, -0.9, 0);
 	  
 	  m.translateTo(-0.7, -0.9);
 	  glUniformMatrix4fv(mat, 1, GL_FALSE, m.getData()); 
 
-	  glDrawArrays(GL_TRIANGLES, num_of_triangles*3+3, 3);	
+	  glDrawArrays(GL_TRIANGLES, num_of_vertices*3+3, 3);	
 	  float time =glfwGetTime();
 
 	  /*
@@ -418,13 +312,12 @@ int main () {
 	  squareX = 0.4 * std::cos(time*speedSquare);   
 	  squareY= 0.4 * std::sin(time*speedSquare);
 
-	  //move(translationMatrix, squareX, squareY, 0);
 	  m.translateTo(squareX, squareY);
 
 	  glUniformMatrix4fv(mat, 1, GL_FALSE, m.getData()); 
 
 
-	  glDrawArrays(GL_TRIANGLES, num_of_triangles*3+6, 6);
+	  glDrawArrays(GL_TRIANGLES, num_of_vertices*3+6, 6);
 	 //  facem swap la buffere (Double buffer)
 	  glfwSwapBuffers(window);
 
