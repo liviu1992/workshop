e7e25e3a6bd63e1794e9014927cd7edfc7713854
liviu1992 <miron_liviu92@yahoo.com>
2014-07-12 18:36:04 +0300

    probabil la fel ca versiunea anterioara

--- Project_2d/data/shaders/fragmentShaderSquare.glsl
+++
@@ -1,7 +0,0 @@
-#version 400
-uniform sampler2D diffuse;
-in vec2 texCoord0;
-out vec4 frag_colour;
-void main () {
-	frag_colour = texture2D(diffuse, texCoord0); 
-}
--- Project_2d/data/shaders/vertexShaderT.glsl
+++
@@ -1,9 +0,0 @@
-#version 400
- in vec3 vertex_position;
-uniform mat4 Matrix;
-void main () 
-{
-
-	 gl_Position = Matrix * vec4(vertex_position,1.0) ;
-	
-}
